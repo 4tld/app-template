e5c0a02fc8ff3537c9b41ce6442b49d511ecb69d
Archibald Perez <archieperez99@gmail.com>
2022-07-18 15:43:09 +0200

    :tada: Initial template

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <RouterView />
+  <div
+    v-if="error"
+    class="box error"
+  >
+    An error occurred: {{ error.message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterView } from 'vue-router'
+
+export default defineComponent({
+  components: {
+    RouterView,
+  },
+
+  data () {
+    return {
+      error: null as null|Error,
+    }
+  },
+
+  errorCaptured (error) {
+    this.error = error as Error
+  },
+})
+</script>
---
+++ src/components/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex">
+    <img
+      width="48"
+      src="/favicon.svg"
+    />
+    <h1>&nbsp;changeme</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {}
+  },
+
+  methods: {},
+})
+</script>
